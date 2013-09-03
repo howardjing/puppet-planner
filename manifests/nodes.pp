707657a6cd4a87a9c1a3e3b7507d5d50d5e8e394
Howard Jing <howard.jing@gmail.com>
2013-09-03 17:10:37 -0400

    including nodejs for puppetclient

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -4,6 +4,11 @@ node 'puppetmaster' {
 }
 
 node 'puppetclient' {
+
+  # nodejs for a javascript runtime for rails
+  include nodejs
+
+  # install rbenv + set up capistrano
   rails::application { 'planner':
     user => 'deployer',
     ruby => '2.0.0-p247'
