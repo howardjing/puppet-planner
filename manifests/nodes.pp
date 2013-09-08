b1c8c7aa16384e7ae2e80db91e301e7902c10fca
Howard Jing <howard.jing@gmail.com>
2013-09-07 22:39:55 -0400

    adding a temporary default site that returns a 404

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,13 +1,5 @@
-
 node 'puppetmaster' {
 	include git
-}
-
-node 'hjing' {
-  class { 'planner':
-    server_name       => 'planner.hjing.me', 
-    rails_environment => 'production'
-  }  
 }
 
 node 'puppetclient' {
@@ -15,4 +7,25 @@ node 'puppetclient' {
     server_name       => 'staging.planner.dev',
     rails_environment => 'staging'
   }  
+
+  # temporarily have default site just return a 404
+  nginx::resource::vhost { '_':
+    location_custom_cfg => {
+      return       => '404'
+    }
+  }
+}
+
+node 'hjing' {
+  class { 'planner':
+    server_name       => 'planner.hjing.me', 
+    rails_environment => 'production'
+  }  
+
+  # temporarily have default site just return a 404
+  nginx::resource::vhost { '_':
+    location_custom_cfg => {
+      return       => '404'
+    }
+  }
 }
